f3f30fa84f823e694e503a512232512a99025052
damccorm <damccorm@microsoft.com>
2019-09-27 14:01:53 -0400

    Error handling

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 async function run() {
+  try {
     const ymlPath = core.getInput('configuration-path', {required: true});
     const token = core.getInput('repo-token', {required: true});
+
     let fileContents: string = fs.readFileSync(ymlPath, {encoding: 'utf8'});
 
     if (github.context.payload.action !== 'opened') {
@@ -20,72 +22,71 @@ async function run() {
       return;
     }
 
-    try {
-      if (!fileContents) {
-        console.log('No file contents found in the rules file.')
-        return;
-      }
-
-      let issueRules: irm.IIssueRules = irm.parseYamlContents(fileContents);
-      let eng: irm.RuleEngine = new irm.RuleEngine();
-
-      // Just look at most recent issues
-      const client = new github.GitHub(token);
-
-      // TODO - we probably should be able to get this from context
-      const { data: issue } = await client.issues.get({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: github.context.issue.number
-      });
-
-      if (!issue.pull_request) {
-        let results: irm.ITagResults = eng.processRules(issue.body, issueRules.rules);
-
-        if (results.labelsToAdd && results.labelsToAdd.length == 0) {
-          results = eng.processRules(issue.body, issueRules.noMatches);
-        }
-
-        eng.processTags(results.labelsToAdd, issueRules.tags);
-
-        if (results.labelsToAdd.length > 0) {
-          console.log('Adding labels', JSON.stringify(results.labelsToAdd));
-        }
-        else {
-          console.log('No labels to add');
-        }
-
-        if (results.assigneesToAdd.length > 0) {
-          console.log('Adding assignees', JSON.stringify(results.assigneesToAdd));
-        }
-        else {
-          console.log('No assignees to add');
-        }
-
-        let labels: string[] = [];
-        issue.labels.forEach((label: any) => {
-          labels.push(label.name);
-        })
-        labels = labels.concat(results.labelsToAdd);
-
-        let assignees: string[] = [];
-        issue.assignees.forEach((assignee: any) => {
-          assignees.push(assignee.login);
-        })
-        assignees = assignees.concat(results.assigneesToAdd);
-
-        await client.issues.update({
-          owner: github.context.repo.owner,
-          repo: github.context.repo.repo,
-          issue_number: issue.number,
-          labels: labels,
-          assignees: assignees
-        });
-      }
+    if (!fileContents) {
+      console.log('No file contents found in the rules file.')
+      return;
     }
-    catch (err) {
-      // TODO: figure out a good tracing / logging story
+
+    let issueRules: irm.IIssueRules = irm.parseYamlContents(fileContents);
+    let eng: irm.RuleEngine = new irm.RuleEngine();
+
+    // Just look at most recent issues
+    const client = new github.GitHub(token);
+
+    // TODO - we probably should be able to get this from context
+    const { data: issue } = await client.issues.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.issue.number
+    });
+
+    console.log('Got issue', issue.number);
+
+    let results: irm.ITagResults = eng.processRules(issue.body, issueRules.rules);
+
+    if (results.labelsToAdd && results.labelsToAdd.length == 0) {
+      results = eng.processRules(issue.body, issueRules.noMatches);
     }
+
+    eng.processTags(results.labelsToAdd, issueRules.tags);
+
+    if (results.labelsToAdd.length > 0) {
+      console.log('Adding labels', JSON.stringify(results.labelsToAdd));
+    }
+    else {
+      console.log('No labels to add');
+    }
+
+    if (results.assigneesToAdd.length > 0) {
+      console.log('Adding assignees', JSON.stringify(results.assigneesToAdd));
+    }
+    else {
+      console.log('No assignees to add');
+    }
+
+    let labels: string[] = [];
+    issue.labels.forEach((label: any) => {
+      labels.push(label.name);
+    })
+    labels = labels.concat(results.labelsToAdd);
+
+    let assignees: string[] = [];
+    issue.assignees.forEach((assignee: any) => {
+      assignees.push(assignee.login);
+    })
+    assignees = assignees.concat(results.assigneesToAdd);
+
+    await client.issues.update({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: issue.number,
+      labels: labels,
+      assignees: assignees
+    });
+  }
+  catch (err) {
+    core.setFailed(err);
+  }
 }
 
 run();
